8ab5bb45d95726c8e60d30fcddbd1ac086435f1d
ed <edwardhaddican@gmail.com>
2023-06-13 09:59:20 -0400

    added basic routing prior to demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
+import { useState } from "react";
+import NavBar from "./components/NavBar";
+import AllProducts from "./components/AllProducts";
+import { Routes, Route } from "react-router-dom";
 
-import './App.css'
+import "./App.css";
 
 function App() {
- 
-
   return (
     <div>
       <h1>Welcome to Strangers Things</h1>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<AllProducts />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/AllProducts.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllProducts = () => {
+  return (
+    <div>
+      <h2>I am all products</h2>
+    </div>
+  );
+};
+
+export default AllProducts;
